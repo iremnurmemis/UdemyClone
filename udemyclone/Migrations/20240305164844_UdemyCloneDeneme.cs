5ef7872dcb52a72060dedfcf86a2e41aaa61638a
iremnurmemis <iremnurmemis123@gmail.com>
2024-03-05 19:59:25 +0300

    .net mvc ile UdemyClone

---
+++ udemyclone/Migrations/20240305164844_UdemyCloneDeneme.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proje1.Migrations
+{
+    /// <inheritdoc />
+    public partial class UdemyCloneDeneme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    Idd = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ad = table.Column<string>(type: "TEXT", nullable: true),
+                    Soyad = table.Column<string>(type: "TEXT", nullable: true),
+                    OgrenciMi = table.Column<int>(type: "INTEGER", nullable: true),
+                    Eposta = table.Column<string>(type: "TEXT", nullable: false),
+                    Sifre = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.Idd);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogretmenler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ad = table.Column<string>(type: "TEXT", nullable: true),
+                    Soyad = table.Column<string>(type: "TEXT", nullable: true),
+                    Eposta = table.Column<string>(type: "TEXT", nullable: true),
+                    Sifre = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogretmenler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resimler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Ad = table.Column<string>(type: "TEXT", nullable: true),
+                    Resim = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resimler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videolar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Baslik = table.Column<string>(type: "TEXT", nullable: true),
+                    VideoData = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    Kategori = table.Column<string>(type: "TEXT", nullable: true),
+                    Aciklama = table.Column<string>(type: "TEXT", nullable: true),
+                    Resim = table.Column<byte[]>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videolar", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+
+            migrationBuilder.DropTable(
+                name: "Ogretmenler");
+
+            migrationBuilder.DropTable(
+                name: "Resimler");
+
+            migrationBuilder.DropTable(
+                name: "Videolar");
+        }
+    }
+}
